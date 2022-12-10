1773bcc52d982a84807129c4f11d637cfc96b007
d.jiryaee <d.jiryaee@arshinco.com>
2022-12-10 17:07:11 +0330

    add chatGatWay

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chat } from './chat.entity';
+import { ChatGatway } from './chat.gatway';
+import { ChatService } from './chat.service';
 
-@Module({})
+@Module({
+    imports:[TypeOrmModule.forFeature([Chat])],
+    providers: [ChatGatway,ChatService]
+})
 export class ChatModule {}
