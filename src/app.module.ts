e1a2f5c9da96bf43daa1c4a3b5add74cdef827bf
d.jiryaee <d.jiryaee@arshinco.com>
2022-12-10 14:42:19 +0330

    add typeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import { typeOrmAsync } from './config/typeOrm.config';
 import { validatinConfig } from './config/validation.config';
 
 @Module({
-  imports: [ConfigModule.forRoot(validatinConfig),ChatModule],
+  imports: [
+    TypeOrmModule.forRootAsync(typeOrmAsync),
+    ConfigModule.forRoot(validatinConfig),
+    ChatModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
